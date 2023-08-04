e98663144987889f267a54a5db32086d35ea9829
Pedro Dias <pedromndias@gmail.com>
2023-08-04 12:04:39 +0200

    initial files

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,1 +1,16 @@
 // We can create a movie schema & model here
+
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: String,
+  director: String,
+  duration: String,
+  genre: [String],
+  rate: String
+})
+
+const Movie = mongoose.model("Movie", movieSchema)
+
+module.exports = Movie
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-// Connects to the database... if we had one :( 
-// TODO                                        
+// Config the config:
+require("dotenv").config()
+console.log(process.env.CLAVE_SUPER_SECRETA);
 
+// Connects to the database... if we had one :(           
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/my-first-db")
+.then(() => {
+  console.log("Server connected to DB");
+})
+.catch((error) => {
+  console.log(error);
+})
 
 // Handles http requests (express is node js framework)
 const express = require('express');
@@ -15,11 +25,15 @@ const hbs = require("hbs");
 app.use(express.static("public"))
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views/' )
-
+const morgan = require("morgan")
+app.use(morgan("dev"))
+const favicon = require("serve-favicon")
+app.use(favicon(__dirname + "/public/images/favicon.ico"))
 
 // Local Variables 
 // TODO           
 
+const Movie = require("./models/Movie.model")
 
 // 👇 Start handling routes here
 app.get('/', (req, res, next) => {
@@ -31,16 +45,36 @@ app.get('/about', (req, res, next) => {
 })
 
 app.get('/movies', (req, res, next) => {
-  res.render("movies.hbs")
+  Movie.find()
+  .then((response) => {
+    // console.log(response);
+    res.render("movies.hbs", {
+      allMovies: response
+    })
+  })
+  .catch((error) => {
+    // console.log(error);
+    // Go to error handler type 500:
+    next(error);
+  })
+  
 })
 
 
 // To handle errors.
-// TODO            
+// 404 Not Found:
+app.use((req, res, next) => {
+  res.status(404).render("not-found.hbs")
+})
+// 500:
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).render("error.hbs")
+})
 
 
 // Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 
-app.listen(3000, () => {
-  console.log(`Server listening on port http://localhost:${3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port http://localhost:${process.env.PORT}`);
 });
